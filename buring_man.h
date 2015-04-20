2f9a883af94aa4c3b58e99704eb9856c91cd1b8d
himanshu chablani <himanshu.neo92@gmail.com>
2015-04-20 09:39:36 +0100

    fresh build

--- buring_man.h
+++ buring_man.h
@@ -6,9 +6,74 @@
 //
 namespace octet {
   /// Scene containing a box with octet.
+  #define height_image 256
+  #define width_image 256
   class buring_man : public app {
     // scene for drawing box
     ref<visual_scene> app_scene;
+
+    struct my_vertex {
+      vec3p pos;
+      vec3p nor;
+      uint32_t color;
+    };
+    static uint32_t make_color(float r, float g, float b) {
+      return 0xff000000 + ((int)(r*255.0f) << 0) + ((int)(g*255.0f) << 8) + ((int)(b*255.0f) << 16);
+    }    
+
+    void mouse_control(mat4t &camera){
+
+      int x, y;
+      get_mouse_pos(x, y);
+      int vx, vy;
+      get_viewport_size(vx, vy);
+
+      mat4t modelToWorld;
+
+      modelToWorld.loadIdentity();
+      modelToWorld[3] = vec4(camera.w().x(), camera.w().y(), camera.w().z(), 1);
+      modelToWorld.rotateY((float)-x*2.0f);
+      if (vy / 2 - y < 70 && vy / 2 - y > -70)
+        modelToWorld.rotateX((float)vy / 2 - y);
+      if (vy / 2 - y >= 70)
+        modelToWorld.rotateX(70);
+      if (vy / 2 - y <= -70)
+        modelToWorld.rotateX(-70);
+      camera = modelToWorld;//apply to the node
+    }
+
+    void key_presses(mat4t &camera){
+
+      if (is_key_down(key_esc)){
+        exit(0);
+      }
+
+      //camera movement keys (arrow keys and WASD)
+      if (is_key_down(key::key_shift))
+      {
+        camera.translate(0, 5, 0);
+      }
+      if (is_key_down(key::key_ctrl))
+      {
+        camera.translate(0, -5, 0);
+      }
+      if (is_key_down('W'))
+      {
+        camera.translate(0, 0, -5);
+      }
+      if (is_key_down('S'))
+      {
+        camera.translate(0, 0, 5);
+      }
+      if (is_key_down('A'))
+      {
+        camera.translate(-5, 0, 0);
+      }
+      if (is_key_down('D'))
+      {
+        camera.translate(5, 0, 0);
+      }
+    }
   public:
     /// this is called when we construct the class before everything is initialised.
     buring_man(int argc, char **argv) : app(argc, argv) {
@@ -18,12 +83,15 @@ namespace octet {
     void app_init() {
       app_scene =  new visual_scene();
       app_scene->create_default_camera_and_lights();
+      app_scene->get_camera_instance(0)->set_far_plane(10000);
+      mat4t &camera_mat = app_scene->get_camera_instance(0)->get_node()->access_nodeToParent();
+      camera_mat.translate(0, 50, 0);
+      camera_mat.rotateY(-180);
+      camera_mat.rotateX(0);
 
-      material *red = new material(vec4(1, 0, 0, 1));
-      mesh_box *box = new mesh_box(vec3(4));
-      scene_node *node = new scene_node();
-      app_scene->add_child(node);
-      app_scene->add_mesh_instance(new mesh_instance(node, box, red));
+      
+
+      
     }
 
     /// this is called to draw the world
@@ -37,11 +105,9 @@ namespace octet {
 
       // draw the scene
       app_scene->render((float)vx / vy);
-
-      // tumble the box  (there is only one mesh instance)
-      scene_node *node = app_scene->get_mesh_instance(0)->get_node();
-      node->rotate(1, vec3(1, 0, 0));
-      node->rotate(1, vec3(0, 1, 0));
+      mat4t &cam=app_scene->get_camera_instance(0)->get_node()->access_nodeToParent();
+      mouse_control(cam);
+      key_presses(cam);
     }
   };
 }
